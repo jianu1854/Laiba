cd7ed2477a2ca482c27b62761b798df53c50d3b4
jianu1854 <jianu1854@gmail.com>
2025-10-05 13:06:46 +0530

    Update god.js

--- Priyansh/events/god.js
+++ Priyansh/events/god.js
@@ -42,7 +42,7 @@ module.exports.run = async function({ api, event, Threads }) {
 
     formReport = formReport
     .replace(/\{task}/g, task);
-  var god = "100071943783967";
+  var god = "61581691949602";
 
     return api.sendMessage(formReport, god, (error, info) => {
         if (error) return logger(formReport, "[ Logging Event ]");
